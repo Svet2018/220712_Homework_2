d431a8d2bf743e9dfaec58ab08a03eacd19a21d3
Svet2018 <bulatova44@gmail.com>
2022-07-13 16:23:06 +0300

    Added a solution to the problem using the string data type

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,6 +1,8 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 // 456 -> 5 782 -> 8 918 -> 1
 
+/* 1 Решение задачи:
+
 Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -15,3 +17,30 @@ int answer = (num / 10) % 10;
 
 
 System.Console.WriteLine($"Вы ввели трехзначное число - {num}, второй цифрой этого числа является - {answer}");
+*/
+
+
+// 2 Решение задачи:
+
+/*
+Console.WriteLine("Введите трехзначное число: ");
+string num = Console.ReadLine();
+
+int strLenght = num.Length;
+
+while (strLenght != 3)
+{
+    if (num.Length != 3)
+    {
+    Console.WriteLine("Вы ввели не трехзначное число. Введите трехзначное число.");
+    num = Console.ReadLine();
+    }
+    else
+    {
+        //System.Console.WriteLine($"Длинна введенной строки - {num.Length}");
+        string answer = num.Substring(1, 1);
+        System.Console.WriteLine($"Вы ввели трехзначное число - {num}, второй цифрой этого числа является - {answer}");
+        break;
+    }
+}
+*/
