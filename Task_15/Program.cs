c1b4983f59d75b192c135cf6c64f395192651f52
Svet2018 <bulatova44@gmail.com>
2022-07-13 22:03:40 +0300

    Added a solution to Task_15

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -1,2 +1,27 @@
 ﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да 7 -> да 1 -> нет
+
+/* 1 Решение задачи:
+
+Console.WriteLine("Введите в числовом виде день недели от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string[] weekDays= {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+int weekDayaArrayLenght = weekDays.Length;
+int correctNum = num - 1;
+string answer = weekDays[correctNum];
+
+if (num == 6 ^ num == 7)
+{
+    System.Console.WriteLine($"Вы ввели число - {num}, что соответствует такому дню недели как - {weekDays[correctNum]} и является выходным днем");
+}
+else
+{
+    System.Console.WriteLine($"Вы ввели число - {num}, что соответствует такому дню недели как - {weekDays[correctNum]} и является рабочим днем");
+}
+
+*/
+
+
+
+
+
