5142f43922d2918e300299ba25128471d42291e1
Svet2018 <bulatova44@gmail.com>
2022-07-16 20:50:52 +0300

    Added solution to task 15

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да 7 -> да 1 -> нет
 
-/* 1 Решение задачи:
+// 1 Решение задачи:
+
+/*
 
 Console.WriteLine("Введите в числовом виде день недели от 1 до 7: ");
 int num = Convert.ToInt32(Console.ReadLine());
 string[] weekDays= {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
-int weekDayaArrayLenght = weekDays.Length;
 int correctNum = num - 1;
-string answer = weekDays[correctNum];
 
 if (num == 6 ^ num == 7)
 {
@@ -22,13 +22,36 @@ else
 */
 
 
-// 2 Решение задачи:
+// 2 Решение задачи с помощью метода dayOfWeek:
 
-Console.WriteLine("Введите в числовом виде день недели от 1 до 7: ");
-string num = Console.ReadLine();
-string[] weekDays= {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+/*
 
-int strLenght = num.Length;
+void dayOfWeek(int day)
+{
+    string[] weekDays = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+    string answer = weekDays[day - 1];
+    if (day == 6 ^ day == 7)
+    {
+        System.Console.WriteLine($"Дано число - {day}, что соответствует такому дню недели как - {answer} и является выходным днем");
+    }
+        else
+    {
+        System.Console.WriteLine($"Дано число - {day}, что соответствует такому дню недели как - {answer} и является рабочим днем");
+    }
+}
+
+dayOfWeek(1);
+dayOfWeek(2);
+dayOfWeek(3);
+dayOfWeek(4);
+dayOfWeek(5);
+dayOfWeek(6);
+dayOfWeek(7);
+
+*/
+
+
+
 
 
 
