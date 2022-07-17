c88a9c65cde327cdada96488f45dbfd05ca97b49
Svet2018 <bulatova44@gmail.com>
2022-07-17 16:47:02 +0300

    The final version of the solution to task No. 13

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -24,43 +24,16 @@ while (number > 999)
 number = number % 10;
 
 System.Console.WriteLine($"Вы ввели число {num}, третьей цифрой данного числа является - {number}");
+
 */
 
-// 2 Решение задачи с помощью метода ThirdNum()
 
-// // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 //645 -> 5 78 -> третьей цифры нет 32679 -> 6
 
-void ThirdNum(int num)
-{
-    int number;
-    int answer;
-    if (num < 100 & num > 0)
-    {
-        Console.WriteLine($"Число {num} третьей цифры не имеет");
-    }
-    number = num;  
-    
-     while (num > 999)
-    {
-        number = number / 10;
-    }
-    answer = number % 10;
-    System.Console.WriteLine($"Дано число - {num}, третьей цифрой этого числа является - {answer}");
-    return;      
-}
-
-ThirdNum(59367);
-ThirdNum(12);
-ThirdNum(214);
-ThirdNum(12367);
-
-
-// 3 Решение задачи с помощью типа string:
-
+// 2 Решение задачи с помощью типа string:
 
 /*
-
 Console.WriteLine("Введите число:");
 string num = Console.ReadLine();
 
@@ -75,5 +48,4 @@ else
     string answer = num.Substring(2, 1);
     System.Console.WriteLine($"Вы ввели число - {num}, третьей цифрой этого числа является - {answer}");       
 }
-
 */
